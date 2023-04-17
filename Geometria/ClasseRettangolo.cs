c667311902d3235e88eb0cd8eaeb8ca3599a1808
JordanSgrigna <kaliwyrdiscord@gmail.com>
2023-04-17 15:15:47 +0200

    Rectangle class + user input and validation

---
+++ Geometria/ClasseRettangolo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+	public class Rettangolo
+	{
+		// ATTRIBUTI
+		public int baseRettangolo;
+		public int altezzaRettangolo;
+
+		// STATI
+
+		// COSTRUTTORE
+		public Rettangolo(int baseRettangolo,  int altezzaRettangolo)
+		{
+			this.baseRettangolo = baseRettangolo;
+			this.altezzaRettangolo = altezzaRettangolo;
+		}
+
+		// METODI
+		public int calcolaPerimetro(int baseRettangolo, int altezzaRettangolo)
+		{
+			int perimetro = (this.baseRettangolo * 2) + (this.altezzaRettangolo * 2);
+			return perimetro;
+
+		}
+
+
+		public int calcolaArea(int baseRettangolo, int altezzaRettangolo)
+		{
+
+			int area = this.baseRettangolo * this.altezzaRettangolo;
+			return area;
+
+		}
+	}
+}
--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using Geometria;
+
+Console.Write("Scrivi l'altezza del rettangolo: ");
+int baseRettangolo = int.Parse(Console.ReadLine());
+
+Console.Write("Scrivi la lunghezza del rettangolo: ");
+int altezzaRettangolo = int.Parse((Console.ReadLine()));
+
+while((baseRettangolo == altezzaRettangolo) || ((baseRettangolo == 0) || (altezzaRettangolo == 0)))
+{
+	Console.Write("Riscrivi l'altezza del rettangolo diversa dalla lunghezza o da 0: ");
+	baseRettangolo = int.Parse(Console.ReadLine());
+
+	Console.Write("Scrivi la lunghezza del rettangolo diversa dalla lunghezza o da 0: ");
+	altezzaRettangolo = int.Parse((Console.ReadLine()));
+}
+
+Rettangolo rettangoloUno = new Rettangolo(baseRettangolo, altezzaRettangolo);
